de4f46f7cf532bd847d33cf59c310b1697b3e4f4
Michael Montesano <michaelnmontesano@gmail.com>
2025-11-16 16:17:08 -0500

    Update device lookup: hide results until user selects filters and fetch, default limit to 500

---
+++ Vertical Terminal iOS/Vertical Terminal iOS/Vertical_Terminal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vertical_Terminal_iOSApp.swift
+//  Vertical Terminal iOS
+//
+//  Created by Michael Montesano on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Vertical_Terminal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
